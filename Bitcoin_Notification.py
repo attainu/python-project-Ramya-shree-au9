636975424245fb39aa77cac667d1c958e56d1109
Ramyashree HC <ramyashreehc21@gmail.com>
2020-09-18 20:39:03 +0530

    project files

---
+++ Bitcoin_Notification.py
@@ -0,0 +1,144 @@
+import requests # Import From Library
+import time
+import json
+import argparse
+from datetime import datetime
+from tqdm import tqdm
+
+# Coinmarket _API_URL
+BITCOIN_API_URL_INR = 'https://api.coindesk.com/v1/bpi/currentprice/INR.json'
+BITCOIN_API_URL_USD = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+# IFTTT_Webhooks_URL
+IFTTT_WEBHOOKS_URL = 'https://maker.ifttt.com/trigger/{}/with/key/NF-CSWQZErEpMx6UJNDlktZQKIw5hRJsoGwChxtnS-'
+
+# Latest Bitcoin Price is Fetched from Bitcoin API.
+def get_latest_bitcoin_price_in_inr():
+    response = requests.get(BITCOIN_API_URL_INR)    
+    response_json = response.json()
+    return(float(response_json['bpi']['INR']['rate_float'])) # (Bitcoin Price Is Get In Indian Rupee)
+
+# Latest Bitcoin Price is Fetched from Bitcoin API.
+def get_latest_bitcoin_price_in_usd():
+    response = requests.get(BITCOIN_API_URL_USD)
+    response_json = response.json()
+    return(float(response_json['bpi']['USD']['rate_float'])) # (Bitcoin Price Is Get In US dollars)
+  
+# Formating The Bitcoin Price Notification in INR.
+def format_bitcoin_price_in_inr(price): 
+    date = datetime.now()
+    date, price = date.strftime('%d.%m.%Y %H:%M'),price
+    row = '{}: ₹ <{}>'.format(date, price)
+    return row  # Format Of Bitcoin Price Is Shows First Date,Time And value Of The Bitcoin
+
+# Formating The Bitcoin Price Notification in USD
+def format_bitcoin_price_in_usd(price): 
+    date = datetime.now()
+    date, price = date.strftime('%d.%m.%Y %H:%M'),price
+    row = '{}: $ <{}>'.format(date, price)
+    return row
+
+# It Post The Message To webhook. 
+def post_ifttt_webhook(value, event):
+    data = {'value1': value}
+    post_event = IFTTT_WEBHOOKS_URL.format(event)
+    requests.post(post_event, json=data) 
+
+# When Choose TELEGRAM  Notification,Push IFTTT Master Driver That Fetches The Data And Post TO App. 
+def telegram_master_driver(alert, time_interval, bitcoin_price, format_bitcoin,BITCOIN_ALERT_LIMIT):
+    print("Welcome :) ")
+    print("Bitcoin Notification Updating")
+    TIME_INTERVAL = float(time_interval[0])
+    x = 1
+    for i in tqdm(range(0,100000)):
+        for x in range(0,1000):
+            x*=4 
+    while True:
+        if bitcoin_price < BITCOIN_ALERT_LIMIT:  # IF Bitcoin Price Is BeloW Alert Limit, Notification Sends To This NUmber - 919535940239
+            print("You Will Receive a SMS in This Number - 919535940239")
+            event = 'Bitcoin_Price_Emergency'
+            post_ifttt_webhook(format_bitcoin, event)
+            
+        else:                # Else Message Sends To telegram
+            print('You will Receive a Text-Message In Telegram')
+            event = 'Latest_Bitcoin_Price_Alert'
+            post_ifttt_webhook(format_bitcoin, event)
+
+        time.sleep(5 * TIME_INTERVAL)
+
+# When Choose Twitter Notification,Push IFTTT Master Driver That Fetches The Data And Post TO App. 
+def twitter_master_driver(alert, time_interval, bitcoin_price, format_bitcoin, BITCOIN_ALERT_LIMIT):
+    print("Welcome :) ")
+    print("Bitcoin Notification Updating")
+    TIME_INTERVAL = float(time_interval[0])
+    x = 1
+    for i in tqdm(range(0,100000)):
+        for x in range(0,1000):
+            x*=4 
+    while True:
+        if bitcoin_price < BITCOIN_ALERT_LIMIT:     ## IF Bitcoin Price Is BeloW Alert Limit, Notification Sends To This NUmber - 919535940239
+            print("You Will Receive a SMS in This Number - 919535940239")
+            event = 'Bitcoin_Price_Emergency'
+            post_ifttt_webhook(format_bitcoin, event)
+            
+        else:                   # Else Tweets The Twitter Acount
+            print('Plese Follow This Twitter Acount - https://twitter.com/HcRamyashree')
+            event = 'Latest_bitcoin_price'
+            post_ifttt_webhook(format_bitcoin, event)
+        
+        time.sleep(5 * TIME_INTERVAL)
+
+# This Is The Matrix Of This App,Which Takes The Input Throght CLI And Passes It.
+def MainControlFunc():
+    input = argparse.ArgumentParser(
+        description='Bitcoin Price Notification App.', epilog='This Script helps in sharing Real-Time Bitcoin Prices to Appropriate Services')
+
+    input.add_argument('-e', '--alert_amount',default=[836745.00, 10000], # (10000 USD = 736745.00 Indian Rupee) 
+                        help='The price of 1 bitcoin when an emergency alert will be sent. Default is 10000 USD')
+
+    input.add_argument('-t', '--time_interval', default=[60], 
+                        help='The Frequency at which the Bitcoin value is going to be Fetched from Server')
+    
+    input.add_argument('-d', '--destination_app',
+                        help='The Messaging Service Destiation 1. TWITTER 2. ANDROID SMS 3. TELEGRAM', required=True)
+
+    input.add_argument('-c', '--currency',  
+                        help='Bitcoin value in 1.INR (Indian Rupees 2.USD (US Dollars)', required = True)
+
+    input.add_argument('-ct', '--cointype',  default=['Bitcoin'], # (In Cryptocurrency Choosing Bitcoin)
+                        help='Type of crypto currency is bitcoin')
+
+    args = input.parse_args()
+
+    # This Is The Switch Control This Will Call Only That Function That Is Mentioned
+    # By User And Transfer The Control To It.
+    if(args.destination_app == 'TELEGRAM' and args.currency == 'INR'):
+        bitcoin_price = get_latest_bitcoin_price_in_inr()
+        format_bitcoin = format_bitcoin_price_in_inr(bitcoin_price)
+        BITCOIN_ALERT_LIMIT = float(args.alert_amount[0])
+        telegram_master_driver(args.alert_amount,
+                            args.time_interval,bitcoin_price,format_bitcoin,BITCOIN_ALERT_LIMIT)
+    
+    if(args.destination_app == 'TELEGRAM' and args.currency == 'USD'):
+        bitcoin_price = get_latest_bitcoin_price_in_usd()
+        format_bitcoin = format_bitcoin_price_in_usd(bitcoin_price)
+        BITCOIN_ALERT_LIMIT = float(args.alert_amount[1])
+        telegram_master_driver(args.alert_amount,
+                            args.time_interval,bitcoin_price,format_bitcoin,BITCOIN_ALERT_LIMIT)
+
+    if(args.destination_app == 'TWITTER' and args.currency == 'INR'):
+        bitcoin_price = get_latest_bitcoin_price_in_inr()
+        format_bitcoin = format_bitcoin_price_in_inr(bitcoin_price)
+        BITCOIN_ALERT_LIMIT = float(args.alert_amount[0])
+        twitter_master_driver(args.alert_amount,
+                            args.time_interval,bitcoin_price,format_bitcoin,BITCOIN_ALERT_LIMIT)
+
+    if(args.destination_app == 'TWITTER' and args.currency == 'USD'):
+        bitcoin_price = get_latest_bitcoin_price_in_usd()
+        format_bitcoin = format_bitcoin_price_in_usd(bitcoin_price)
+        BITCOIN_ALERT_LIMIT = float(args.alert_amount[1])
+        twitter_master_driver(args.alert_amount,
+                            args.time_interval,bitcoin_price,format_bitcoin,BITCOIN_ALERT_LIMIT)
+   
+   
+if __name__ == '__main__':
+    MainControlFunc()   # Main Function
---
+++ README.md
@@ -0,0 +1,102 @@
+# BITCOIN PRICE NOTIFICATION                  
+## BITCOIN PROJECT:
+![alt text](https://images.app.goo.gl/UHsPdRLrhUxpryHT8)
+______
+## About:-
+____
+    As we all know, Crypto Currency price is a fickle thing.You never really know where it’s going 
+    to be at the end of the day.  So,instead of constantly checking various sites for the latest updates,
+    this python program push crypto currency price notifications at certain time interval and also notify 
+    when price reach to a certain value as threshold value provided by user.
+
+## Advantage Of Bitcoin
+______
+    Standard wire transfers and foreign purchases typically involve fees and exchange costs. Since bitcoin 
+    transactions have no intermediary institutions or government involvement, the costs of transacting are kept very low.
+    This can be a major advantage for travelers.
+
+## Installation Guide:
+____________
+#### step by step series of examples and explanations about how to get your development env running.
+
+### Step 1
+* Either Download the Repo,Which you will find at the top right of Repo link or clone it in your Local Machine.
+
+       git clone https://github.com/attainu/python-project-Ramya-shree-au9
+
+### Step 2
+* Go into the Folder by using command line. 
+
+### Step 3
+* Make Sure that your Machine has Python 3.8+
+Run the follwing Command
+
+        $ python3 Bitcoin_Notification.py --help
+
+### Step 4
+* Then, you will see the following options
+
+        Optional arguments:
+
+        -h, --help show this help message exit
+
+        -e ALERT_AMOUNT, --alert_amount ALERT_AMOUNT
+                        The price of 1 bitcoin when an emergency 
+                        alert will be sent. Default is 10000 USD
+
+        -t TIME_INTERVAL, --time_interval TIME_INTERVAL
+                        The Frequency at which the Bitcoin value
+                        is going to be Fetched from Server
+
+        -d DESTINATION_APP,   --destination_app DESTINATION_APP
+                        The Messaging Service Destiation 
+                        1. TWITTER 2. IFTTT 3. TELEGRAM
+        -c CURRENCY, --currency CURRENCY
+                        Bitcoin value in 1.Indian Rupees(INR) 2.USD(US
+                        Dollars)
+        -ct COINTYPE, --cointype COINTYPE
+                        Type of crypto currency is bitcoin
+
+### Step 5
+* If user didn't give above optional argument then python will consider below default parameter
+
+    * alert_amount = 10000 
+    * time_interval = 60 
+    * coinType = 'Bitcoin'
+    * currency = user’s choice out of the 2 option 1.INR (Indian Rupees) 2.USD (US Dollars)
+    * For destination_app update will send to any app of the user’s choice out of the 2 option. 1. TWITTER   2. TELEGRAM
+
+* After sends a notification to a Particular app, if the price does not cross the threshold limit it sends Android 
+  SMS (particular phone no.).Otherwise a normal price update will send to the user's choice out of the 2 option.
+
+## Target Applications:
+_____
+![Test Image 1](Capture.JPJ)
+
+## Python Packages & Libraries Used
+________
+* Request
+* time
+* Argparser
+* Tqdm (For Progress bar)
+## Technologies Used:
+______
+* Python 3.8
+* HTTPS
+* Webhooks
+* Messaging Platforms Available:
+    * Telegram
+    * IFTTT App
+    * Twitter
+    * Android SMS
+
+## Future Scope:
+____
+* Expand the Messaging Platforms
+* Include other Cryptocurrency for this Project.
+
+## API Reference:
+____
+* [Cinedesk Docs](https://www.coindesk.com/coindesk-api)
+* [IFTTT API reference](https://platform.ifttt.com/docs/api_reference)
+
